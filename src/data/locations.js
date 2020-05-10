538f99d64cbc71ebbfbd925de52a30678c8f9dcb
peter westman <pwestman@gmail.com>
2020-05-11 00:35:39 +0200

    add locations

---
+++ src/data/locations.js
@@ -0,0 +1,24 @@
+export const locations = [
+  {
+    placename: ‘Herndon, VA’,
+    date: ‘August 1, 2015’,
+    location: {
+      lat: 38.958988,
+      lng: -77.417320
+    },
+    todo: [
+      ‘Where we start! 🚀’
+    ]
+  },
+  {
+    placename: ‘Middlesboro, KY',
+    date: ‘August 1, 2015’,
+    location: {
+      lat: 36.627517,
+      lng: -83.621635
+    },
+    todo: [
+      ‘Cumberland Gap 🌳’
+    ]
+  }
+];
